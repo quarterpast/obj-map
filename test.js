33f5e5e92deb6e8ff9a125ae84e6551c9dc6902c
Matt Brennan <mattyb1000@gmail.com>
2014-10-05 16:59:54 +0100

    async tests

--- test.js
+++ test.js
@@ -35,3 +35,43 @@ describe('objMap', function() {
 		})).to.eql({a:2, b:4, c:6});
 	});
 });
+
+describe('objMap.async', function() {
+	it('should call iterator with each key', function(done) {
+		var iter = sinon.spy();
+		objMap.async({a:1, b:2, c:3}, iter, function(e) {
+			expect(iter).was.calledThrice();
+			expect(iter).was.calledWith('a');
+			expect(iter).was.calledWith('b');
+			expect(iter).was.calledWith('c');
+			done(e);
+		});
+	});
+	it('should call iterator with each key and value', function(done) {
+		var iter = sinon.spy();
+		objMap.async({a:1, b:2, c:3}, iter, function(e) {
+			expect(iter).was.calledThrice();
+			expect(iter).was.calledWith('a', 1);
+			expect(iter).was.calledWith('b', 2);
+			expect(iter).was.calledWith('c', 3);
+			done(e);
+		});
+	});
+	it('should call back new object with values transformed by iterator', function(done) {
+		objMap.async({a:1, b:2, c:3}, function(k, v) {
+			return v * 2;
+		}, function(e, ys) {
+			expect(ys).to.eql({a:2, b:4, c:6});
+			done(e);
+		});
+	});
+	it('should exit on first error', function(done) {
+		var o = {};
+		var iter = sinon.stub().yields(o);
+		objMap.async({a:1, b:2, c:3}, iter, function(e, ys) {
+			expect(e).to.be(o);
+			expect(iter).was.calledOnce();
+			done();
+		});
+	});
+});
